ba0a4ce8a60f1d893ffb62290490eacea8550757
Asrul Azwan <asrulazwan90@gmail.com>
2024-06-16 13:23:12 +0800

    feat: Add property model and controller

---
+++ controllers/property-controller.js
@@ -0,0 +1,66 @@
+import propertyModel from '../models/property-model.js';
+
+const propertyController = {
+    createProperty: (req, res) =>
+    {
+        try 
+        {
+            const { title, description, price } = req.body;
+            const property = propertyModel.createProperty({ title, description, price });
+        
+            res.status(201).json({ message: 'Property created successfully', data: property });
+        } 
+        catch (error) 
+        {
+            res.status(500).json({ error: 'Error creating property' });
+        }
+    },
+    getPropertyList: (req, res) =>
+    {
+        const propertyList = propertyModel.getPropertyList();
+        res.json({ message: 'get property list', data: propertyList });
+    },
+    getPropertyById: (req, res) =>
+    {
+        const propertyId = req.params.id;
+        const property = propertyModel.getPropertyById(propertyId);
+    
+        if (!property) 
+        {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+    
+        res.json({ message: `get property id ${propertyId}`, data: property });
+    },
+    updateProperty: (req, res) => 
+    {
+        const newValue = req.body;
+        const propertyId = req.params.id;
+        const property = propertyModel.getPropertyById(propertyId);
+    
+        if (!property) 
+        {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        propertyModel.updateProperty(property, newValue);
+
+        res.json({ message: `Property with id ${propertyId} updated successfully` });
+    },
+    deleteProperty: (req, res) => 
+    {
+        const propertyId = req.params.id;
+        const property = propertyModel.getPropertyById(propertyId);
+    
+        if (!property) 
+        {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        propertyModel.deleteProperty(property);
+
+        res.json({ message: `Property with id ${propertyId} deleted successfully` });
+    },
+}
+
+export default propertyController
---
+++ models/property-model.js
@@ -0,0 +1,37 @@
+import generateUniqueId from '../utils/unique-id.js';
+
+const properties = [];
+
+const propertyModel = {
+    createProperty: (propertyEntity) =>
+    {
+        propertyEntity.id = generateUniqueId();
+        properties.push(propertyEntity);
+
+        return propertyEntity;
+    },
+    getPropertyList: () =>
+    {
+        return properties;
+    },
+    getPropertyById: (propertyId) =>
+    {
+        return properties.find((prop) => prop.id === propertyId);
+    },
+    updateProperty: (property, newValue) =>
+    {
+        property.title = newValue.title;
+        property.description = newValue.description;
+        property.price = newValue.price;
+    },
+    deleteProperty: (property) =>
+    {
+        const indexToRemove = properties.findIndex((prop) => prop.id === property.id);
+        if (indexToRemove !== -1) 
+        {
+            properties.splice(indexToRemove, 1);
+        }
+    },
+}
+
+export default propertyModel
