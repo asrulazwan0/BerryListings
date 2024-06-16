e601baeeca8279a9bd4fa440692a26121955f716
Asrul Azwan <asrulazwan90@gmail.com>
2024-06-16 09:11:24 +0800

    feat: Add open for automatic open browser

--- swagger.js
+++ swagger.js
@@ -1,33 +1,36 @@
+import open from 'open';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Property Listings API',
-      version: '1.0.0',
-      description: 'API documentation for property listings',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000', // Your server URL
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // Specify the path to your route files
-};
-
-const specs = swaggerJsdoc(options);
 function swaggerDocs(app, port) 
 {
-    // Swagger Page
-    app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
-    // Documentation in JSON format
-    app.get('/docs.json', (req, res) => {
-      res.setHeader('Content-Type', 'application/json')
-      res.send(specs)
-    })
+  const url = `http://localhost:${port}`;
+  const swaggerPath = '/docs';
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Property Listings API',
+        version: '1.0.0',
+        description: 'API documentation for property listings',
+      },
+      servers: [
+        {
+          url: url,
+        },
+      ],
+    },
+    apis: ['./routes/*.js'],
+  };
+  const specs = swaggerJsdoc(options);
+
+  app.use(swaggerPath, swaggerUi.serve, swaggerUi.setup(specs))
+  app.get('/docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(specs)
+  })
+
+  open(url + swaggerPath);
 }
 
 export default swaggerDocs
