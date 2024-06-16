290321ee8e281d455b76901000d6ff9f8a1f4498
Asrul Azwan <asrulazwan90@gmail.com>
2024-06-17 01:26:23 +0800

    feat: Update property controller to support new propertyModel

--- src/controllers/property-controller.js
+++ src/controllers/property-controller.js
@@ -1,68 +1,84 @@
-import propertyModel from '../utils/models/property-model.js';
+import propertyModel from '../models/property-model.js';
 
 const propertyController = {
-    createProperty: (req, res) =>
+    createProperty: async (req, res) =>
     {
         try 
         {
             const { title, description, price } = req.body;
+            const numericPrice = parseFloat(price);
         
-            // Validate the data (optional step)
+            const result = await propertyModel.createProperty({ title, description, price: numericPrice });
         
-            const property = propertyModel.createProperty({ title, description, price });
-        
-            res.status(201).json({ message: 'Property created successfully', data: property });
+            res.status(201).json({ message: 'Property created successfully', data: result });
         } 
         catch (error) 
         {
             res.status(500).json({ error: 'Error creating property' });
         }
     },
-    getPropertyList: (req, res) =>
+    getPropertyList: async (req, res) =>
     {
-        const propertyList = propertyModel.getPropertyList();
+        const propertyList = await propertyModel.getPropertyList();
         res.json({ message: 'get property list', data: propertyList });
     },
-    getPropertyById: (req, res) =>
+    getPropertyById: async (req, res) =>
     {
-        const propertyId = req.params.id;
-        const property = propertyModel.getPropertyById(propertyId);
+        const { id } = req.params;
+        const property = await propertyModel.getPropertyByUuid(id);
     
         if (!property) 
         {
             return res.status(404).json({ message: 'Property not found' });
         }
     
-        res.json({ message: `get property id ${propertyId}`, data: property });
+        res.json({ message: `get property id ${id}`, data: property });
     },
-    updateProperty: (req, res) => 
+    updateProperty: async (req, res) => 
     {
-        const newValue = req.body;
-        const propertyId = req.params.id;
-        const property = propertyModel.getPropertyById(propertyId);
-    
-        if (!property) 
+        const { id } = req.params;
+
+        try 
         {
-            return res.status(404).json({ message: 'Property not found' });
+            const { title, description, price } = req.body;
+            const numericPrice = parseFloat(price);
+            const property = await propertyModel.getPropertyByUuid(id);
+        
+            if (!property) 
+            {
+                return res.status(404).json({ message: 'Property not found' });
+            }
+
+            const result = await propertyModel.updateProperty(property, { title, description, price: numericPrice });
+
+            res.json({ message: `Property with id ${id} updated successfully`, data: result });
         }
-
-        propertyModel.updateProperty(property, newValue);
-
-        res.json({ message: `Property with id ${propertyId} updated successfully` });
+        catch (error) 
+        {
+            console.log(error)
+            res.status(500).json({ error: `Error updating property id ${id}` });
+        }
     },
-    deleteProperty: (req, res) => 
+    deleteProperty: async (req, res) => 
     {
-        const propertyId = req.params.id;
-        const property = propertyModel.getPropertyById(propertyId);
-    
-        if (!property) 
+        const { id } = req.params;
+
+        try 
         {
-            return res.status(404).json({ message: 'Property not found' });
+            const property = await propertyModel.getPropertyByUuid(id);
+        
+            if (!property) 
+            {
+                return res.status(404).json({ message: 'Property not found' });
+            }
+
+            await propertyModel.deleteProperty(property.uuid);
+
+            res.status(204).send();} 
+        catch (error) 
+        {
+            res.status(500).json({ error: `Error deleting property id ${id}` });
         }
-
-        propertyModel.deleteProperty(property);
-
-        res.json({ message: `Property with id ${propertyId} deleted successfully` });
     },
 }
 
