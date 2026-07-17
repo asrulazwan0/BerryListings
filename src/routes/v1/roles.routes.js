b8098daaff53be4520573bf98a3b8167acbd80df
Asrul Azwan <asrulazwan90@gmail.com>
2026-07-17 12:10:56 +0800

    fix: wrong import path in roles.routes.js

--- src/routes/v1/roles.routes.js
+++ src/routes/v1/roles.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import authMiddleware from '../middlewares/auth.middleware.js';
+import authMiddleware from '../../middlewares/auth.middleware.js';
 import rolesController from '../controllers/roles.controller.js';
 
 const roleRoutes = Router();
